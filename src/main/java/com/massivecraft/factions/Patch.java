7c0f66343b5ef2a1b7863840cf8eba49df083fd9
IamEld3st <iameld3st@gmail.com>
2017-08-18 18:59:33 +0200

    Update some naming...

--- src/main/java/com/massivecraft/factions/Patch.java
+++ src/main/java/com/massivecraft/factions/Patch.java
@@ -3,7 +3,7 @@ package com.massivecraft.factions;
 public class Patch {
 
     public static final String NAME = "FactionsOne";
-    public static final String VERSION = "1.3-SNAPSHOT";
+    public static final String VERSION = "1.3-MINECORE";
     public static final String MAINTAINER = "Sataniel";
     public static final String AUTHORS = "drtshock, RenegadeEagle, externo6, Sataniel and contributors";
 
